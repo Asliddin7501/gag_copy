e0781a4163d6244075c431ee05a9d69ce9036ca8
asliddin7501 <mahmudovasliddin750@gmail.com>
2021-05-02 15:21:25 +0500

    Post yuklash

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from gag.helpers import UploadTo
+from gag.mixins import TranslateMixin
 
-# Create your models here.
+
+class Category(TranslateMixin, models.Model):
+    translate_fileds = ['name']
+
+    name_uz = models.CharField(max_length=50)
+    name_ru = models.CharField(max_length=50)
+    image = models.ImageField(max_length=100, upload_to=UploadTo("category"))
+    class Meta:
+        verbose_name = "Kategoriya"
+        verbose_name_plural = "Kategoriyalar"
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    user = models.ForeignKey("client.User", on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    comment = models.TextField(verbose_name="Izoh")
+    file = models.FileField(max_length=100, verbose_name="Rasm/Video", upload_to=UploadTo("post"))
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PostComment(models.Model):
+    parent = models.ForeignKey('PostComment', on_delete=models.RESTRICT, null=True, default=None)
+    post = models.ForeignKey('main.Post', on_delete=models.RESTRICT)
+    user = models.ForeignKey('client.User', on_delete=models.RESTRICT)
+    comment = models.TextField(default=None, null=True)
+    image = models.ImageField(max_length=100, upload_to=UploadTo("comment"))
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,28 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import View, CreateView
+from django.utils.translation import gettext_lazy as _
+from main.models import Post
 
 
 class MainIndex(View):
     def get(self, request):
         return render(request, 'main/index.html')
+
+
+class UploadPost(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['category', 'comment', 'file']
+    template_name = 'layouts/form.html'
+    success_url = reverse_lazy('main:upload')
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+
+        request.title = _("Yuklash")
+
+    def form_valid(self, form):
+        messages.success(self.request, _("Muvaffaqiyatli qo'shildi."))
+        return super().form_valid(form)
